b4fec64f4407e946cc510b3cad303e00b0b1a92f
Егор Михеев <45826858+Tanma145@users.noreply.github.com>
2020-03-12 20:33:25 +0300

    Update TList.h

--- mp2-lab5-list/mp2-lab5-list/TList.h
+++ mp2-lab5-list/mp2-lab5-list/TList.h
@@ -8,7 +8,7 @@ struct TNode {
 	TNode<T>* pNext;
 };
 
-//СПИСОК
+//Г‘ГЏГ€Г‘ГЋГЉ
 template <class T>
 class TList{
 protected:
@@ -162,7 +162,7 @@ public:
 };
 
 
-//ОЧЕРЕДЬ
+//ГЋГ—Г…ГђГ…Г„Гњ
 template <class T>
 class TQueue {
 	TNode<T>* pFirst, * pLast;
@@ -217,7 +217,7 @@ public:
 		p->pNext = pMax->pNext;
 		pMax->pNext = p;
 	}
-	/* эта функция противоречит логике очереди
+	/* ГЅГІГ  ГґГіГ­ГЄГ¶ГЁГї ГЇГ°Г®ГІГЁГўГ®Г°ГҐГ·ГЁГІ Г«Г®ГЈГЁГЄГҐ Г®Г·ГҐГ°ГҐГ¤ГЁ
 	friend ostream& operator<< (ostream &os, const TQueue<T> &q) {
 		TNode<T>* pCur = q.Head();
 		while (pCur) {
@@ -228,120 +228,3 @@ public:
 	}
 	*/
 };
-<<<<<<< HEAD
-=======
-//MONOMIAL
-struct TMonomial {
-	double coeff;
-	int px, py, pz;
-	bool operator== (TMonomial a) {
-		return ((px == a.px) && (py == a.py) && (pz == a.pz));
-	}
-	bool operator> (TMonomial a) {
-		return ((px > a.px) || (px <= a.px) && (py > a.py) || (px <= a.px) && (py <= a.py) && (pz > a.pz));
-	}
-	bool operator< (TMonomial a) {
-		return ((px < a.px) || (px >= a.px) && (py < a.py) || (px >= a.px) && (py >= a.py) && (pz < a.pz));
-	}
-	bool operator>= (TMonomial a) {
-		return ((px >= a.px) || (px < a.px) && (py >= a.py) || (px < a.px) && (py < a.py) && (pz >= a.pz));
-	}
-	bool operator<= (TMonomial a) {
-		return ((px <= a.px) || (px > a.px) && (py <= a.py) || (px > a.px) && (py > a.py) && (pz <= a.pz));
-	}
-	void operator+= (TMonomial mon) {
-		if (*this == mon) {
-			coeff += mon.coeff;
-		}
-		else
-			throw 0;
-	}
-	TMonomial operator+ (TMonomial mon) {
-		TMonomial res = *this;
-		res += mon;
-		return res;
-	}
-};
-//POLYNOMIAL
-class TPolynomial : public THeadList<TMonomial> {
-public:
-	TPolynomial() : THeadList<TMonomial>(){
-		pHead->Val.pz = -1;
-	}
-	TPolynomial(TPolynomial& p) : THeadList<TMonomial>() {
-		pHead->Val.pz = -1;
-		for (p.Reset(); !p.IsEnd(); p.GoNext()) {
-			TMonomial mon = p.DelCurr->val;
-			InsLast(mon);
-		}
-	}
-	TPolynomial(int arr[][2], int size) :THeadList<TMonomial>(){
-		pHead->Val.pz = -1;
-		for (int i = 0; i < size; i++) {
-			TMonomial mon;
-			mon.coeff = arr[i][0];
-			mon.px = arr[i][1] / 100;
-			mon.py = arr[i][1] / 10 % 10;
-			mon.pz = arr[i][1] %10;
-			InsLast(mon);
-		}
-	}
-	void operator+= (TMonomial& mon) {
-		for (Reset(); !IsEnd(); GoNext()) {
-			if (pCurr->Val == mon) {
-				pCurr->Val += mon;
-				if (!pCurr->Val.coeff)
-					DelCurr();
-				return;
-			}
-			if (mon > pCurr->Val) {
-				InsCurr(mon);
-				return;
-			}
-			InsLast(mon);
-		}
-	}
-	TPolynomial operator+ (TMonomial& mon) {
-		TPolynomial res = *this;
-		res += mon;
-		return res;
-	}
-	void operator*= (TMonomial& mon) {
-		for (Reset(); !IsEnd(); GoNext()) {
-			pCurr->Val.coeff *= mon.coeff;
-			pCurr->Val.px += mon.px;
-			pCurr->Val.py += mon.py;
-			pCurr->Val.pz += mon.pz;
-		}
-	}
-	TPolynomial operator* (TMonomial& mon) {
-		TPolynomial res = *this;
-		res *= mon;
-		return res;
-	}
-	void operator+= (TPolynomial& q) {
-		TMonomial pm, qm, rm;
-		Reset();
-		q.Reset();
-		while (!q.IsEnd) {
-			pm = pCurr->Val;
-			qm = q.pCurr->Val;
-			if (pm > qm)
-				GoNext();
-			else
-				if (pm < qm) {
-					InsCurr(qm);
-				}
-				else {
-					rm = pm;
-					rm.coeff += qm.coeff;
-					pCurr->Val = rm;
-					if (rm.coeff == 0)
-						DelCurr();
-					GoNext();
-					q.GoNext();
-				}
-		}
-	}
-};
->>>>>>> 9d7be9bf96894807693b26adc4aaf15cda67476e
